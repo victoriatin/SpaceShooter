7807abae91518d9aaa35ca539f10a46236760613
victoriatin <52183531+victoriatin@users.noreply.github.com>
2019-07-21 14:57:12 -0400

    Finished Tutorial

---
+++ SpaceShooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float lifetime;
+    void Start()
+    {
+        Destroy (gameObject, lifetime);
+    }
+
+    
+}
---
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+     public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+     public float speed;
+     public float tilt;
+     public Boundary boundary;
+     public GameObject shot;
+     public Transform shotSpawn;
+     public float fireRate;
+     public AudioSource musicSource;
+    private float nextFire;
+     private Rigidbody rb;
+
+     private void Start()
+     {
+         musicSource = GetComponent<AudioSource>();
+          rb = GetComponent<Rigidbody>();
+     }
+    void Update ()
+    { //change to "Jump" for spacebar shooting 
+        if (Input.GetButton("Fire1") && Time.time > nextFire){
+            nextFire = Time.time + fireRate;
+          //  GameObject clone = 
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+            GetComponent<AudioSource>().Play ();
+        }
+       
+    }
+     void FixedUpdate()
+     {
+          float moveHorizontal = Input.GetAxis("Horizontal");
+          float moveVertical = Input.GetAxis("Vertical");
+
+          Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+          rb.velocity = movement * speed;
+
+          rb.position = new Vector3
+          (
+               Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+               0.0f,
+               Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+          );
+
+          rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+     }
+}
